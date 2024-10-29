588517560521c687b5b278ec41ec2df456190853
R0bert-BLN <robybalan04@gmail.com>
2024-10-29 17:54:18 +0200

    Adaugare rezolvare

--- main.cpp
+++ main.cpp
@@ -1,32 +1,67 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <vector>
 
 using namespace std;
+
+struct Problem
+{
+    string idProblem;
+    string speciality;
+};
+
+struct Doctor
+{
+    string idDoctor;
+    string speciality;
+};
 
 int main()
 {
     ifstream inFile("input.txt");
 
-    int no_problems, no_doctors;
-    string name, speciality;
-    
+    int no_problems;
+    int no_doctors;
+    bool ok;
+
     inFile >> no_problems;
+    vector<Problem> problems(no_problems);
 
     for (int i = 0; i < no_problems; i++)
     {
-        inFile >> name;
-        inFile >> speciality;
-        cout << name << ' ' << speciality << '\n';
+        inFile >> problems[i].idProblem;
+        inFile >> problems[i].speciality;
     }
 
     inFile >> no_doctors;
+    vector<Doctor> doctors(no_doctors);
 
     for (int i = 0; i < no_doctors; i++)
     {
-        inFile >> name;
-        inFile >> speciality;
-        cout << name << ' ' << speciality << '\n';
+        inFile >> doctors[i].idDoctor;
+        inFile >> doctors[i].speciality;
+    }
+
+    for (int i = 0; i < no_problems; i++)
+    {
+        ok = false;
+
+        for (int j = 0; j < no_doctors; j++)
+        {
+            if (problems[i].speciality == doctors[j].speciality)
+            {
+                cout << problems[i].idProblem << " Acceptat" << endl;
+                ok = true;
+
+                break;
+            }
+        }
+
+        if (!ok)
+        {
+            cout << problems[i].idProblem << " Respins" << endl;
+        }
     }
 
     return 0;
