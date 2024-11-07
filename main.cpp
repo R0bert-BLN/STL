4bf24a5090a4d38292f59ccf46dd01bf60f57eec
R0bert-BLN <robybalan04@gmail.com>
2024-11-07 18:23:45 +0200

    update

--- main.cpp
+++ main.cpp
@@ -7,62 +7,69 @@ using namespace std;
 
 struct Problem
 {
-    string idProblem;
-    string speciality;
+	string idProblem;
+	string speciality;
 };
 
 struct Doctor
 {
-    string idDoctor;
-    string speciality;
+	string idDoctor;
+	string speciality;
 };
 
 int main()
 {
-    ifstream inFile("input.txt");
+	ifstream inFile("input2.txt");
 
-    int no_problems;
-    int no_doctors;
-    bool ok;
+	if (!inFile.is_open())
+	{
+		cerr << "Eroare la deschiderea fisierului" << endl;
 
-    inFile >> no_problems;
-    vector<Problem> problems(no_problems);
+		return 1;
+	}
 
-    for (int i = 0; i < no_problems; i++)
-    {
-        inFile >> problems[i].idProblem;
-        inFile >> problems[i].speciality;
-    }
+	int no_problems;
+	int no_doctors;
+	bool ok;
 
-    inFile >> no_doctors;
-    vector<Doctor> doctors(no_doctors);
+	inFile >> no_problems;
+	vector<Problem> problems(no_problems);
 
-    for (int i = 0; i < no_doctors; i++)
-    {
-        inFile >> doctors[i].idDoctor;
-        inFile >> doctors[i].speciality;
-    }
+	for (int i = 0; i < no_problems; i++)
+	{
+		inFile >> problems[i].idProblem;
+		inFile >> problems[i].speciality;
+	}
 
-    for (int i = 0; i < no_problems; i++)
-    {
-        ok = false;
+	inFile >> no_doctors;
+	vector<Doctor> doctors(no_doctors);
 
-        for (int j = 0; j < no_doctors; j++)
-        {
-            if (problems[i].speciality == doctors[j].speciality)
-            {
-                cout << problems[i].idProblem << " Acceptat" << endl;
-                ok = true;
+	for (int i = 0; i < no_doctors; i++)
+	{
+		inFile >> doctors[i].idDoctor;
+		inFile >> doctors[i].speciality;
+	}
 
-                break;
-            }
-        }
+	for (int i = 0; i < no_problems; i++)
+	{
+		ok = false;
 
-        if (!ok)
-        {
-            cout << problems[i].idProblem << " Respins" << endl;
-        }
-    }
+		for (int j = 0; j < no_doctors; j++)
+		{
+			if (problems[i].speciality == doctors[j].speciality)
+			{
+				cout << problems[i].idProblem << " Acceptat" << endl;
+				ok = true;
 
-    return 0;
+				break;
+			}
+		}
+
+		if (!ok)
+		{
+			cout << problems[i].idProblem << " Respins" << endl;
+		}
+	}
+
+	return 0;
 }
