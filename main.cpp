64903d75a8a6b5398d99ed2fc6d40eb21b4ad264
R0bert-BLN <robybalan04@gmail.com>
2024-11-26 23:37:23 +0200

    Implement solution for STL #3

--- main.cpp
+++ main.cpp
@@ -2,8 +2,8 @@
 #include <fstream>
 #include <string>
 #include <vector>
-#include <cctype>
 #include <algorithm>
+#include <queue>
 
 using namespace std;
 
@@ -11,22 +11,31 @@ struct Problem
 {
 	string idProblem;
 	string speciality;
+	int time;
+	int priority;
+
+	bool operator<(const Problem& otherProblem) const
+	{
+		return priority < otherProblem.priority;
+	}
 };
 
 struct Doctor
 {
 	string idDoctor;
 	string speciality;
+	string problemsList = "";
+	int time = 8;
+	int solvedProblems = 0;
 };
 
 int main()
 {
-	ifstream file("input.txt");
+	ifstream file("input2.txt");
 
 	if (!file.is_open())
 	{
 		cerr << "Error opening file" << endl;
-
 		return 1;
 	}
 
@@ -34,11 +43,13 @@ int main()
 	int noDoctors;
 
 	file >> noProblems;
-	vector<Problem> problems(noProblems);
+	priority_queue<Problem> problems;
 
 	for (int i = 0; i < noProblems; i++)
 	{
-		file >> problems[i].idProblem >> problems[i].speciality;
+		Problem problem;
+		file >> problem.idProblem >> problem.speciality >> problem.time >> problem.priority;
+		problems.push(problem);
 	}
 
 	file >> noDoctors;
@@ -49,20 +60,31 @@ int main()
 		file >> doctors[i].idDoctor >> doctors[i].speciality;
 	}
 
-	for_each(begin(doctors), end(doctors), [&problems](Doctor& doctor)
-		{
-			auto it = find_if(begin(problems), end(problems), [&doctor](Problem& problem)
-				{
-					return problem.speciality == doctor.speciality;
-				});
+	while (!problems.empty())
+	{
+		Problem problem = problems.top();
+		problems.pop();
 
-			if (it != end(problems))
+		auto it = find_if(begin(doctors), end(doctors), [&problem](Doctor& doctor)
 			{
-				cout << doctor.idDoctor << " " << it->idProblem << endl;
+				return (problem.speciality == doctor.speciality && problem.time <= doctor.time);
+			});
 
-				problems.erase(it);
-			}
-		});
+		if (it != end(doctors))
+		{
+			it->problemsList += problem.idProblem + " ";
+			it->solvedProblems++;
+			it->time -= problem.time;
+		}
+	}
+
+	for (const Doctor& doctor : doctors)
+	{
+		if (doctor.solvedProblems != 0)
+		{
+			cout << doctor.idDoctor << " " << doctor.solvedProblems << " " << doctor.problemsList << endl;
+		}
+	}
 
 	return 0;
 }
